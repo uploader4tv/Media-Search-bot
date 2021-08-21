b4137543557e5c1390483b43e4640a392d521097
uploader4tv <88353584+uploader4tv@users.noreply.github.com>
2021-08-21 09:21:42 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('Request Films', url='https://t.me/uploader4films')
+            InlineKeyboardButton('Request Shows', url='https://t.me/tvseriesreq')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
